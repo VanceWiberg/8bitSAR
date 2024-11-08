9a2c2d4eb7fc141fe77ad491be2a3894b5f009ce
VanceWiberg <vancewiberg@gmail.com>
2024-11-07 19:59:23 -0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -38,5 +38,78 @@ module tb ();
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
+initial begin 
+  ui_in = 8'b00000010;
+  uio_in = 8'b0;
+  clk = 1'b0;
+  rst_n = 1'b1;
+  ena = 1'b1;
+	while(1) begin
+  	#1 clk = ~clk;
+	end
+end
+
+initial begin
+	rst_n = 1'b0;
+	repeat(2) @(posedge clk);
+	rst_n = 1'b1;
+	repeat(100) @(posedge clk);
+	$finish(1);
+end
+
+reg [2:0] counter = 3'b111;
+reg [7:0] compare = 8'b10000000;
+reg overlap = 1'b1;
+reg incorrect = 1'b0;
+
+always @(posedge clk) begin
+if (uio_out[0]) begin
+	//$display("");
+	//$display(uo_out, " THIS IS THE SAR         ", compare, " THIS IS THE TEST");
+	if (uo_out == compare) begin
+		//$display("Success");
+	end
+	else begin
+		$display("***INCORRECT***");
+		incorrect = 1'b1;
+	end
+	rst_n = 1'b0;
+	counter = 3'b111;
+	compare = 8'b10000000;
+	overlap = 1'b1;
+end
+else begin
+	
+	incorrect = 1'b0;
+	rst_n = 1'b1;
+	ui_in [0] = $random;
+
+	if((ui_in[0] == 1'b1) && (counter != 3'b0)) begin
+		compare[counter - 1] = 1'b1;
+	end
+	else if (overlap == 1'b1) begin
+		if (ui_in[0]) begin
+		compare[counter] = 1'b1; end
+		else begin
+		compare[counter] = 1'b0; end
+		if (counter != 3'b0) begin
+			compare[counter - 1] = 1'b1;
+		end
+	end
+	
+	if(counter != 3'b0) begin
+		counter = counter - 3'b001;
+	end
+	else begin
+		overlap = 1'b0;
+	end	
+end
+end
+
+//handle simulation
+initial begin
+  $dumpfile("sar8_tb.vcd");  
+  $dumpvars(0, template_tb);
+end	
   );
 endmodule
